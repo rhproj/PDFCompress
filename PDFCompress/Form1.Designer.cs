46e66ab5f624974791c3e2271fbb1b31b8d2b29a
Rh Proj <r.proj@hotmail.com>
2021-06-11 12:01:08 +0300

    Add project files.

---
+++ PDFCompress/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PDFCompress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxPDFs = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCompress = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxPDFs
+            // 
+            this.listBoxPDFs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPDFs.BackColor = System.Drawing.Color.DarkBlue;
+            this.listBoxPDFs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxPDFs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxPDFs.ForeColor = System.Drawing.SystemColors.Info;
+            this.listBoxPDFs.FormattingEnabled = true;
+            this.listBoxPDFs.ItemHeight = 20;
+            this.listBoxPDFs.Location = new System.Drawing.Point(-2, 1);
+            this.listBoxPDFs.Name = "listBoxPDFs";
+            this.listBoxPDFs.Size = new System.Drawing.Size(510, 322);
+            this.listBoxPDFs.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.FlatAppearance.BorderSize = 2;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Yellow;
+            this.btnAdd.Location = new System.Drawing.Point(5, 336);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 59);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            this.btnAdd.MouseEnter += new System.EventHandler(this.OnMouseEnter);
+            this.btnAdd.MouseLeave += new System.EventHandler(this.OnMouseLeave);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(0, 321);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(510, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::PDFCompress.Properties.Resources.EllipsisY;
+            this.pictureBox1.Location = new System.Drawing.Point(138, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCompress.BackColor = System.Drawing.Color.Transparent;
+            this.btnCompress.FlatAppearance.BorderSize = 2;
+            this.btnCompress.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCompress.ForeColor = System.Drawing.Color.Yellow;
+            this.btnCompress.Location = new System.Drawing.Point(371, 338);
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(127, 59);
+            this.btnCompress.TabIndex = 6;
+            this.btnCompress.Text = "Сжать";
+            this.btnCompress.UseVisualStyleBackColor = false;
+            this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+            this.btnCompress.MouseEnter += new System.EventHandler(this.OnMouseEnter2);
+            this.btnCompress.MouseLeave += new System.EventHandler(this.OnMouseLeave2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(505, 401);
+            this.Controls.Add(this.btnCompress);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBoxPDFs);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сжать PDF файлы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxPDFs;
+        private System.Windows.Forms.Button btnAdd;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCompress;
+    }
+}
+
---
+++ PDFCompress/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFCompress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnCompress.MouseEnter += OnMouseEnter2;
+            btnCompress.MouseLeave += OnMouseLeave2;
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            ChangeBtnColor1(btnAdd);
+        }
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            ChangeBtnColor2(btnAdd);
+        }
+        private void OnMouseEnter2(object sender, EventArgs e)
+        {
+            ChangeBtnColor1(btnCompress);
+        }
+        private void OnMouseLeave2(object sender, EventArgs e)
+        {
+            ChangeBtnColor2(btnCompress);
+        }
+        private void ChangeBtnColor1(Button btn)
+        {
+            btn.BackColor = Color.Yellow; 
+            btn.ForeColor = Color.MidnightBlue;
+        }
+        private void ChangeBtnColor2(Button btn)
+        {
+            btn.BackColor = Color.Transparent;
+            btn.ForeColor = Color.Yellow;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog oFD = new OpenFileDialog())
+            {
+                oFD.Multiselect = true;
+                oFD.Title = "Выберите PDF файл";
+                oFD.Filter = "PDF фалы|*.pdf";
+
+                if (oFD.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var f in oFD.FileNames)
+                    {
+                        listBoxPDFs.Items.Add(f);
+                    }
+                }
+            }
+        }
+
+        /// <summary> Method that uses ghost script to compress PDF files </summary>
+        /// <param name="InputFile">our initial PDF file</param>
+        /// <param name="OutPutFile">result of compression</param>
+        /// <param name="CompressValue">how far we go..</param>
+        private bool CompressPDF(string InputFile, string OutPutFile, string CompressValue)
+        {
+            try
+            {
+                Process proc = new Process();
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.CreateNoWindow = true;
+                psi.ErrorDialog = false;
+                psi.UseShellExecute = false;
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                psi.FileName = string.Concat(Path.GetDirectoryName(Application.ExecutablePath), "\\ghost.exe"); //where's the ghost exe dwells
+
+                string args = "-sDEVICE=pdfwrite -dCompatibilityLevel=1.4" + " -dPDFSETTINGS=/" + CompressValue + " -dNOPAUSE  -dQUIET -dBATCH" + " -sOutputFile=\"" + OutPutFile + "\" " + "\"" + InputFile + "\""; //ghostscript settings
+
+                psi.Arguments = args;
+
+                //start the execution
+                proc.StartInfo = psi;
+
+                proc.Start();
+                proc.WaitForExit();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        string outputDir = string.Empty; //to save compressed PDFs into
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string pdffile in listBoxPDFs.Items) //compres everything that listed
+            {
+                string outp = outputDir + "\\" + Path.GetFileNameWithoutExtension(pdffile) + $" (Сжатый).pdf";
+
+                CompressPDF(pdffile, outp, "screen");
+
+                if (progressBar1.InvokeRequired)
+                {
+                    progressBar1.Invoke(new Action(() => progressBar1.Increment(1)));
+                }
+                else
+                {
+                    progressBar1.Increment(1);
+                }
+            }
+        }
+
+        private void btnCompress_Click(object sender, EventArgs e)
+        {
+            if (listBoxPDFs.Items.Count == 0)
+            {
+                MessageBox.Show("Добавьте файлы формата PDF для сжатия!");
+            }
+            else
+            {
+                FolderBrowserDialog fdialog = new FolderBrowserDialog();
+                if (fdialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputDir = fdialog.SelectedPath;
+                }
+
+                progressBar1.Maximum = listBoxPDFs.Items.Count;
+
+                btnCompress.Enabled = false;
+                pictureBox1.Visible = true;
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            listBoxPDFs.Items.Clear();
+            btnCompress.Enabled = true;
+            pictureBox1.Visible = false;
+            MessageBox.Show("Готово!", "Сжатие PDF файла", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Application.Exit();
+        }
+    }
+}
